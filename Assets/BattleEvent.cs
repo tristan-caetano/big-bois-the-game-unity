5665a116073411069bbb066408b1f776b03528b3
tristan-caetano <tristancostacaetano@gmail.com>
2023-11-14 19:16:39 -0500

    Added UI Elements

--- Assets/BattleEvent.cs
+++ Assets/BattleEvent.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
 
@@ -12,6 +13,11 @@ public class BattleEvent : MonoBehaviour
 
     public Transform enemyLoc;
     public Transform playerLoc;
+
+    Unit playerUnit;
+    Unit enemyUnit;
+
+    public Text dialogueText;
 
     public BattleState state;
 
@@ -26,8 +32,13 @@ public class BattleEvent : MonoBehaviour
 
     void SetupBattle(){
         
-        Instantiate(player, playerLoc);
-        Instantiate(enemy, enemyLoc);
+        GameObject playerGO = Instantiate(player, playerLoc);
+        playerUnit = playerGO.getComponent<Unit>();
+
+        GameObject enemyGO = Instantiate(enemy, enemyLoc);
+        enemyUnit = enemyGO.getComponent<Unit>();
+
+        dialogueText.text = "Oh gorsh " + enemyUnit.unitName + " has appeared!";
 
     }
 }
